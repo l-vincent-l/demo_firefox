c709eccb05b7564570b951f1008ed3287db284a5
Vincent Lara <lara.vincent@gmail.com>
2014-11-21 17:29:26 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, url_for
+import requests, json
+app = Flask(__name__)
+
+
+class NavitiaRequest:
+    navitia_token = 'mysecrettoken'
+    base_url = "https://api.navitia.io/"
+    base_url_region = base_url+"v1/coverage/{}"
+
+    @classmethod
+    def get(cls, region, api, parameters):
+        headers = {'Authorization' : cls.navitia_token}
+        url = cls.base_url_region.format(region)
+        url += "/{}"
+        url = url.format(api)
+        print url
+        return requests.get(url, params=parameters, headers=headers)
+
+@app.route('/places')
+def places():
+    query = request.args.get("q")
+    req_ = NavitiaRequest.get("fr-idf", "places",
+            parameters={"q": query})
+
+    if req_.status_code == 200:
+        names = map(lambda a: {"name": a["name"], "id": a["id"]}, req_.json()['places'])
+        return json.dumps(names, indent=4, separators=(',', ': '))
+    return '[]'
+
+
+@app.route('/journeys')
+def journeys():
+    from_ = request.args.get('from')
+    to_ = request.args.get('to')
+    req_ = NavitiaRequest.get("fr-idf", "journeys",
+            parameters={"from":from_, "to": to_})
+    if req_.status_code == 200:
+        return json.dumps(req_.json()['journeys'][0]['sections'], indent=4, separators=(',', ': '))
+    return '{"suggestion":[]}'+str(req_.status_code)
+
+@app.route('/')
+@app.route('/index.html')
+def root():
+    return app.send_static_file('index.html')
+
+@app.route('/map.html')
+def map_route():
+    return app.send_static_file('map.html')
+
+import os
+@app.route('/js/<path:path>')
+def static_proxy(path):
+    # send_static_file will guess the correct MIME type
+    return app.send_static_file(os.path.join('js', path))
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/js/autocomplete.js
@@ -0,0 +1,72 @@
+var from_ = "", to_ = "";
+autofunc = function(field_name, select_func) {
+    $("#autocomplete_"+field_name).autocomplete({
+        source: function(request, response) {
+            $.ajax({
+                type: "get",
+                url: "/places",
+                dataType: "json",
+                data: {
+                    q: request.term,
+                },
+                success: function(data) {
+                    response(
+                        $.map(data, function(item) {
+                            return {
+                                label: item.name,
+                        value: item.name,
+                        id: item.id
+                            }
+                        }))
+                }
+            })
+        },
+            minLength: 2,
+            select: select_func,
+            open: function() {
+                $(this).removeClass("ui-corner-all").addClass("ui-corner-top");
+            },
+            close: function() {
+                $(this).removeClass("ui-corner-top").addClass("ui-corner-all");
+            }
+    });
+}
+
+call_journeys = function() {
+    $.ajax({
+        type: "get",
+    url: "journeys",
+    dataType: "json",
+    data: {
+        from: from_, to: to_
+    },
+    success: function(data) {
+        var line_points = [];
+        var featureGroup = L.featureGroup().addTo(map_);
+        $.map(data, function(section) {
+            line_points = [];
+            if (!section.hasOwnProperty("geojson")) {
+                return;
+            }
+            if (!section.geojson.hasOwnProperty("coordinates")) {
+                return;
+            }
+            $.map(section.geojson.coordinates, function(point) { line_points.push([point[1], point[0]]); });
+            var polyline_options = {
+                color: '#000'
+            };
+            L.polyline(line_points, polyline_options).addTo(map_);
+        });
+    }
+    });
+}
+
+$(document).ready(function () {
+    autofunc("from", function(event, ui){from_ = ui.item.id});
+    autofunc("to", function(event, ui){to_ = ui.item.id; call_journeys();});
+
+    L.mapbox.accessToken = 'pk.eyJ1IjoibC12aW5jZW50LWwiLCJhIjoiaDJfM05UMCJ9.l9oR075SSzJY9hXEqaRvoQ';
+    map_ = L.mapbox.map('map', 'examples.map-i86nkdio')
+    .setView([48.8, 2.3], 11);
+
+});
